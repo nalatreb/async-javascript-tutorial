f01d64fb33a380f6ad1f9f49bd25a04e2d35da11
BertalanV <bertalan.verebelyi@gmail.com>
2020-11-11 23:09:52 +0100

    promises

--- sandbox.js
+++ sandbox.js
@@ -1,25 +1,48 @@
-const getTodos = (callback) => {
-    const request = new XMLHttpRequest();
+const getTodos = (resource) => {
+    return new Promise((resolve, reject) => {
+        const request = new XMLHttpRequest();
 
-    request.addEventListener('readystatechange', () => {
-        if (request.readyState === 4 && request.status === 200) {
-            const data = JSON.parse(request.responseText);
-            callback(undefined, data);
-        } else if (request.readyState === 4) {
-            callback('Could not fetch the data', undefined);
-        }
+        request.addEventListener('readystatechange', () => {
+            if (request.readyState === 4 && request.status === 200) {
+                const data = JSON.parse(request.responseText);
+                resolve(data);
+            } else if (request.readyState === 4) {
+                reject('error getting resource');
+            }
+        });
+    
+        request.open('GET', resource);
+        request.send();
     });
-
-    // request.open('GET', 'https://jsonplaceholder.typicode.com/todos');
-    request.open('GET', 'todos.json');
-    request.send();
 }
 
-getTodos((err, data) => {
-    console.log('callback fired');
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(data);
-    }
-})
+getTodos('todos/berci.json').then(data => {
+    console.log('promise 1 resolved: ', data);
+    return getTodos('todos/icreb.json');
+}).then(data => {
+    console.log('promise 2 resolved: ', data);
+    return getTodos('todos/eetir.json');
+}).then(data => {
+    console.log('promise 3 resolved: ', data);
+}).catch(err => {
+    console.log('promise rejected: ', err);
+});
+
+// const getSomething = () => {
+//     return new Promise((resolve, reject) => {
+//         // resolve('some data');
+//         reject('some error');
+//     });
+// };
+
+// getSomething().then(data => {
+//     console.log(data);
+// }, (err) => {
+//     console.log(err);
+// });
+
+// getSomething().then((data) =>{
+//     console.log(data);
+// }).catch(err => {
+//     console.log(err);
+// });
